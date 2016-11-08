ac4d4588e0acba7b1b6eb6957c1d0aabce082d2f
brandon <mn.cres@gmail.com>
2016-11-08 13:43:05 -0600

    add auth pages

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -9,6 +9,9 @@ import { MessageInputComponent } from './messages/message-input.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthenticationComponent } from './auth/authentication.component';
 import { HeaderComponent } from './header.component';
+import { LogoutComponent } from './auth/logout.component';
+import { SigninComponent } from './auth/signin.component';
+import { SignupComponent } from './auth/signup.component';
 
 @NgModule({
     declarations: [
@@ -18,7 +21,10 @@ import { HeaderComponent } from './header.component';
         MessageInputComponent,
         MessagesComponent,
         AuthenticationComponent,
-        HeaderComponent
+        HeaderComponent,
+        LogoutComponent,
+        SigninComponent,
+        SignupComponent
     ],
     imports: [BrowserModule, FormsModule, routing],
     bootstrap: [AppComponent]

8065e44f4e0ed1287b482fc96f1eddb6598605fc
brandon <mn.cres@gmail.com>
2016-11-22 16:51:29 -0600

    create user back end route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router  = express.Router();
-var message = require('../models/user');
+var User    = require('../models/user');
+var bcrypt  = require('bcrypt');
 
-router.get('/', function (req, res, next) {
-    res.render('index');
+router.post('/', function (req, res, next) {
+    // Create user object from req
+    var user = new User({
+        firstName: req.body.firstName,
+        lastName : req.body.lastName,
+        password : bcrypt.hashSync(req.body.password, 10),
+        email    : req.body.email
+    });
+    user.save(function(err, result) {
+        if(err) {
+            return res.status(500).json({
+                title: 'Error saving user',
+                error: err
+            });
+        }
+        res.status(201).json({
+            message: 'User saved',
+            obj: result
+        });
+    });
 });
 
 module.exports = router;
